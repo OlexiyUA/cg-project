9107bbe1ca6d01e6d1922567411ce132ca7793ec
Oleksii Boreiko <agtevoase@gmail.com>
2020-12-01 15:56:56 +0200

    Beginning uhh

---
+++ mandelbrot.js
@@ -0,0 +1,77 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/6z7GQewK-Ks
+
+var minval = -0.5;
+var maxval = 0.5;
+
+var minSlider;
+var maxSlider;
+
+var frDiv;
+
+let zoom = 2.5;
+let zMin = 0.5;
+let zMax = 5;
+let sens = 0.001;
+
+function setup() {
+  createCanvas(600, 600);
+  pixelDensity(1);
+
+  frDiv = createDiv('');
+}
+
+function draw() {
+  var maxiterations = 100;
+
+  loadPixels();
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      var a = map(x, 0, width, -zoom, zoom);
+      var b = map(y, 0, height, -zoom, zoom);
+
+      var ca = a;
+      var cb = b;
+
+      var n = 0;
+
+      while (n < maxiterations) {
+        var aa = a * a - b * b;
+        var bb = 2 * a * b;
+        a = aa + ca;
+        b = bb + cb;
+        if (a * a + b * b > 16) {
+          break;
+        }
+        n++;
+      }
+
+      var bright = map(n, 0, maxiterations, 0, 1);
+      bright = map(sqrt(bright), 0, 1, 0, 255);
+
+      if (n == maxiterations) {
+        bright = 0;
+      }
+
+      var pix = (x + y * width) * 4;
+      pixels[pix + 0] = bright;
+      pixels[pix + 1] = bright;
+      pixels[pix + 2] = bright;
+      pixels[pix + 3] = 255;
+    }
+  }
+  updatePixels();
+
+}
+
+
+function mouseWheel(event) {
+
+  //move the square according to the vertical scroll amount
+  zoom += sens * event.delta;
+  zoom = constrain(zoom, zMin, zMax);
+  //uncomment to block page scrolling
+  //return false;
+}
