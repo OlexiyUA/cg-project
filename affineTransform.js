a20b00bbf6e9a768470635f5f761ce472b184594
Mykola Lehkyi <lintey8@gmail.com>
2020-12-26 15:16:56 +0200

    Completed affine transformation

--- affineTransform.js
+++ affineTransform.js
@@ -14,134 +14,136 @@ var canvas_height = canvas.height;
 var num_lines_x = Math.floor(canvas_height/grid_size);
 var num_lines_y = Math.floor(canvas_width/grid_size);
 
-// Draw grid lines along X-axis
-for(var i=0; i<=num_lines_x; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    
-    // If line represents X-axis draw in different color
-    if(i == x_axis_distance_grid_lines) 
+function redrawCoordinates(){
+    // Store the current transformation matrix
+
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Restore the transform
+
+        // Draw grid lines along X-axis
+    for(var i=0; i<=num_lines_x; i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        
+        // If line represents X-axis draw in different color
+        if(i == x_axis_distance_grid_lines) 
+            ctx.strokeStyle = "#000000";
+        else
+            ctx.strokeStyle = "#e9e9e9";
+        
+        if(i == num_lines_x) {
+            ctx.moveTo(0, grid_size*i);
+            ctx.lineTo(canvas_width, grid_size*i);
+        }
+        else {
+            ctx.moveTo(0, grid_size*i+0.5);
+            ctx.lineTo(canvas_width, grid_size*i+0.5);
+        }
+        ctx.stroke();
+    }
+
+    // Draw grid lines along Y-axis
+    for(i=0; i<=num_lines_y; i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        
+        // If line represents X-axis draw in different color
+        if(i == y_axis_distance_grid_lines) 
+            ctx.strokeStyle = "#000000";
+        else
+            ctx.strokeStyle = "#e9e9e9";
+        
+        if(i == num_lines_y) {
+            ctx.moveTo(grid_size*i, 0);
+            ctx.lineTo(grid_size*i, canvas_height);
+        }
+        else {
+            ctx.moveTo(grid_size*i+0.5, 0);
+            ctx.lineTo(grid_size*i+0.5, canvas_height);
+        }
+        ctx.stroke();
+    }
+
+    // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+    ctx.translate(y_axis_distance_grid_lines*grid_size, x_axis_distance_grid_lines*grid_size);
+
+    // Ticks marks along the positive X-axis
+    for(i=1; i<(num_lines_y - y_axis_distance_grid_lines); i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
-    else
-        ctx.strokeStyle = "#e9e9e9";
-    
-    if(i == num_lines_x) {
-        ctx.moveTo(0, grid_size*i);
-        ctx.lineTo(canvas_width, grid_size*i);
-    }
-    else {
-        ctx.moveTo(0, grid_size*i+0.5);
-        ctx.lineTo(canvas_width, grid_size*i+0.5);
-    }
-    ctx.stroke();
-}
 
-// Draw grid lines along Y-axis
-for(i=0; i<=num_lines_y; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    
-    // If line represents X-axis draw in different color
-    if(i == y_axis_distance_grid_lines) 
+        // Draw a tick mark 6px long (-3 to 3)
+        ctx.moveTo(grid_size*i+0.5, -3);
+        ctx.lineTo(grid_size*i+0.5, 3);
+        ctx.stroke();
+
+        // Text value at that point
+        ctx.font = '9px Arial';
+        ctx.textAlign = 'start';
+        ctx.fillText(x_axis_starting_point.number*i + x_axis_starting_point.suffix, grid_size*i-2, 15);
+    }
+
+    // Ticks marks along the negative X-axis
+    for(i=1; i<y_axis_distance_grid_lines; i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
-    else
-        ctx.strokeStyle = "#e9e9e9";
-    
-    if(i == num_lines_y) {
-        ctx.moveTo(grid_size*i, 0);
-        ctx.lineTo(grid_size*i, canvas_height);
+
+        // Draw a tick mark 6px long (-3 to 3)
+        ctx.moveTo(-grid_size*i+0.5, -3);
+        ctx.lineTo(-grid_size*i+0.5, 3);
+        ctx.stroke();
+
+        // Text value at that point
+        ctx.font = '9px Arial';
+        ctx.textAlign = 'end';
+        ctx.fillText(-x_axis_starting_point.number*i + x_axis_starting_point.suffix, -grid_size*i+3, 15);
     }
-    else {
-        ctx.moveTo(grid_size*i+0.5, 0);
-        ctx.lineTo(grid_size*i+0.5, canvas_height);
+
+    // Ticks marks along the positive Y-axis
+    // Positive Y-axis of graph is negative Y-axis of the canvas
+    for(i=1; i<(num_lines_x - x_axis_distance_grid_lines); i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#000000";
+
+        // Draw a tick mark 6px long (-3 to 3)
+        ctx.moveTo(-3, grid_size*i+0.5);
+        ctx.lineTo(3, grid_size*i+0.5);
+        ctx.stroke();
+
+        // Text value at that point
+        ctx.font = '9px Arial';
+        ctx.textAlign = 'start';
+        ctx.fillText(-y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, grid_size*i+3);
     }
-    ctx.stroke();
+
+    // Ticks marks along the negative Y-axis
+    // Negative Y-axis of graph is positive Y-axis of the canvas
+    for(i=1; i<x_axis_distance_grid_lines; i++) {
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#000000";
+
+        // Draw a tick mark 6px long (-3 to 3)
+        ctx.moveTo(-3, -grid_size*i+0.5);
+        ctx.lineTo(3, -grid_size*i+0.5);
+        ctx.stroke();
+
+        // Text value at that point
+        ctx.font = '9px Arial';
+        ctx.textAlign = 'start';
+        ctx.fillText(y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, -grid_size*i+3);
+    }
 }
 
-// Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
-ctx.translate(y_axis_distance_grid_lines*grid_size, x_axis_distance_grid_lines*grid_size);
-
-// Ticks marks along the positive X-axis
-for(i=1; i<(num_lines_y - y_axis_distance_grid_lines); i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#000000";
-
-    // Draw a tick mark 6px long (-3 to 3)
-    ctx.moveTo(grid_size*i+0.5, -3);
-    ctx.lineTo(grid_size*i+0.5, 3);
-    ctx.stroke();
-
-    // Text value at that point
-    ctx.font = '9px Arial';
-    ctx.textAlign = 'start';
-    ctx.fillText(x_axis_starting_point.number*i + x_axis_starting_point.suffix, grid_size*i-2, 15);
-}
-
-// Ticks marks along the negative X-axis
-for(i=1; i<y_axis_distance_grid_lines; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#000000";
-
-    // Draw a tick mark 6px long (-3 to 3)
-    ctx.moveTo(-grid_size*i+0.5, -3);
-    ctx.lineTo(-grid_size*i+0.5, 3);
-    ctx.stroke();
-
-    // Text value at that point
-    ctx.font = '9px Arial';
-    ctx.textAlign = 'end';
-    ctx.fillText(-x_axis_starting_point.number*i + x_axis_starting_point.suffix, -grid_size*i+3, 15);
-}
-
-// Ticks marks along the positive Y-axis
-// Positive Y-axis of graph is negative Y-axis of the canvas
-for(i=1; i<(num_lines_x - x_axis_distance_grid_lines); i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#000000";
-
-    // Draw a tick mark 6px long (-3 to 3)
-    ctx.moveTo(-3, grid_size*i+0.5);
-    ctx.lineTo(3, grid_size*i+0.5);
-    ctx.stroke();
-
-    // Text value at that point
-    ctx.font = '9px Arial';
-    ctx.textAlign = 'start';
-    ctx.fillText(-y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, grid_size*i+3);
-}
-
-// Ticks marks along the negative Y-axis
-// Negative Y-axis of graph is positive Y-axis of the canvas
-for(i=1; i<x_axis_distance_grid_lines; i++) {
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#000000";
-
-    // Draw a tick mark 6px long (-3 to 3)
-    ctx.moveTo(-3, -grid_size*i+0.5);
-    ctx.lineTo(3, -grid_size*i+0.5);
-    ctx.stroke();
-
-    // Text value at that point
-    ctx.font = '9px Arial';
-    ctx.textAlign = 'start';
-    ctx.fillText(y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, -grid_size*i+3);
-}
+redrawCoordinates();
 
 var rectangleArray = [[],[],[],[]];
-
-
-// var canvas = document.getElementById("canvasAffineTransform");
-// /** @type {CanvasRenderingContext2D} */
-// var ctx = canvas.getContext("2d");
-
-// ctx.translate(canvas.width/2,canvas.height/2);
-ctx.strokeText('MyText',0,0);
-
-getSquareFromDiagonal();
 
 function getSquareFromDiagonal(){
     let squareDiagonalX1 = parseFloat($('#SquareDiagonalX1').val());
@@ -159,15 +161,121 @@ function getSquareFromDiagonal(){
     return rectangleArray;
 }
 
-function drawRectangleArray(arrayRectangle,color){
+function drawRectangleArray(color){
     ctx.beginPath();
-    ctx.moveTo(arrayRectangle[0][0],arrayRectangle[0][1]);
-    for (let i = 1; i < arrayRectangle.length; i++) {
-        ctx.lineTo(arrayRectangle[i][0],arrayRectangle[i][1]);
+    ctx.moveTo(rectangleArray[0][0]*grid_size,rectangleArray[0][1]*grid_size);
+    for (let i = 1; i < rectangleArray.length; i++) {
+        ctx.lineTo(rectangleArray[i][0]*grid_size,rectangleArray[i][1]*grid_size);
     }
     ctx.closePath();
     ctx.strokeStyle = color;
     ctx.stroke();
 }
 
-drawRectangleArray(rectangleArray,'blue');
+function applyTranslation(){
+    let translationX = parseFloat($('#TranslationX').val());
+    let translationY = parseFloat($('#TranslationY').val());
+    for (let i = 0; i < rectangleArray.length; i++) {
+        rectangleArray[i][0]+=translationX;
+        rectangleArray[i][1]+=translationY;
+    }
+    return rectangleArray;
+}
+
+function applyTranslationOnArray(rectangleArray, translationX,translationY){
+    for (let i = 0; i < rectangleArray.length; i++) {
+        rectangleArray[i][0]+=translationX;
+        rectangleArray[i][1]+=translationY;
+    }
+    return rectangleArray;
+}
+
+function applyScale(){
+    let scaleX = parseFloat($('#ScaleX').val());
+    let scaleY = parseFloat($('#ScaleY').val());
+    let scaleMatrix = [
+                        [scaleX,0],
+                        [0,scaleY]
+                    ];
+    let translationX = rectangleArray[0][0];
+    let translationY = rectangleArray[0][1];
+    applyTranslationOnArray(rectangleArray,-translationX,-translationY)
+    rectangleArray = multiplyMatrix(rectangleArray,scaleMatrix);
+    applyTranslationOnArray(rectangleArray,translationX,translationY)
+    return rectangleArray;
+}
+
+function applyAngle(){
+    let angle = parseFloat($('#Angle').val());
+    let angleRadians = angle * Math.PI /180;
+    var angleMatrix = [
+        [Math.cos(angleRadians), -Math.sin(angleRadians)],
+        [Math.sin(angleRadians), Math.cos(angleRadians)]
+    ]
+    rectangleArray = multiplyMatrix(rectangleArray,angleMatrix);
+    return rectangleArray;
+}
+
+function multiplyMatrix(matrix1, matrix2){
+    let resultMatrix = [[0,0],[0,0],[0,0],[0,0]];
+    for (let i = 0; i < matrix1.length; i++) {
+        for (let j = 0; j < matrix2[0].length; j++) {
+            for (let k = 0; k < matrix1[i].length; k++) 
+            {
+                resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    return resultMatrix;
+} 
+
+$('.numberDiagonal').change(() => {
+        redrawCoordinates();
+        getSquareFromDiagonal();
+        applyTranslation();
+        applyScale();
+        applyAngle();
+        drawRectangleArray('#327f4a');
+    }
+)
+
+$('.numberTranslation').change(() => {
+    redrawCoordinates();
+    getSquareFromDiagonal();
+    applyTranslation();
+    applyScale();
+    applyAngle();
+    drawRectangleArray('#244479');
+}
+)
+
+$('.numberScale').change(() => {
+    redrawCoordinates();
+    getSquareFromDiagonal();
+    applyTranslation();
+    applyScale();
+    applyAngle();
+    drawRectangleArray('#5d237a');
+}
+)
+
+$('.numberAngle').change(() => {
+    redrawCoordinates();
+    getSquareFromDiagonal();
+    applyTranslation();
+    applyScale();
+    applyAngle();
+    drawRectangleArray('#dc6a10');
+}
+)
+
+function redrawRectangle(){
+    redrawCoordinates();
+    getSquareFromDiagonal();
+    applyTranslation();
+    applyScale();
+    applyAngle();
+    drawRectangleArray('#dc6a10');
+}
+
+redrawRectangle();
